28ba04b03e6ccc96491a81fc137a7be3743b8ffc
Roee Nadam <tshompy28@roees-imac.local>
2015-12-22 00:16:38 +0200

    blalba

--- Test1/ViewController.m
+++ Test1/ViewController.m
@@ -19,7 +19,6 @@
 {
     [super viewDidLoad];
     
-    
     // Download the following GitHub repository https://github.com/guilhermechapiewski/yql-ios
     // Copy the yql-ios folder to your project
     
@@ -36,5 +35,38 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+-(void)AddShineAnimationToView:(UIView*)aView
+{
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    [gradient setStartPoint:CGPointMake(0, 0)];
+    [gradient setEndPoint:CGPointMake(1, 0)];
+    gradient.frame = CGRectMake(0, 0, aView.bounds.size.width*3, aView.bounds.size.height);
+    float lowerAlpha = 0.5;
+    gradient.colors = [NSArray arrayWithObjects:
+                       (id)[[UIColor colorWithWhite:1 alpha:1.0] CGColor],
+                       (id)[[UIColor colorWithWhite:1 alpha:lowerAlpha] CGColor],
+                       (id)[[UIColor colorWithWhite:1 alpha:1.0] CGColor],
+                       
+                       nil];
+    gradient.locations = [NSArray arrayWithObjects:
+                          [NSNumber numberWithFloat:0.1],
+                          [NSNumber numberWithFloat:0.5],
+                          [NSNumber numberWithFloat:1.0],
+                          nil];
+    
+    CABasicAnimation *theAnimation;
+    theAnimation=[CABasicAnimation animationWithKeyPath:@"transform.translation.x"];
+    theAnimation.duration = 1;
+    theAnimation.autoreverses = YES;
+    theAnimation.fromValue = [NSNumber numberWithFloat:-aView.frame.size.width*2];
+    theAnimation.toValue = [NSNumber numberWithFloat:0];
+    theAnimation.repeatCount = 2;
+    theAnimation.fillMode = kCAFillModeForwards;
+    theAnimation.removedOnCompletion = YES;
+    [gradient addAnimation:theAnimation forKey:@"animateLayer"];
+    aView.layer.mask = gradient;
+}
+
 
 @end
